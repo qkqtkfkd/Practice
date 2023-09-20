38a542ce0364169bd191ad21818abccba7941aaf
qkqtkfkd <ruby563@hanmail.net>
2023-09-20 10:49:26 +0900

    for문을 이용하여 클래스 제어2

--- 230919/compare/for.js
+++ 230919/compare/for.js
@@ -3,14 +3,21 @@ console.log('for문 연습')
 
 //for(초기화 변수, 조건, 증감)
 //i=undex번호
-// for(let i=4; i<=10; i++){
-//     console.log(`i:${i}`)
-//     for(let j=0; j<=10; j++){
-//         console.log(`j번째:${j}`)
+
+// 일반적인 사례
+// for(let i = 0; i < 10; i++){
+//     console.log(`i : ${i}`)
+// }
+
+// 중첩한 사례
+// for(let i = 0; i < 10; i++){
+//     console.log(`i : ${i}`)
+//     for(let j = 0; j < 10; j++){
+//         console.log(`j번째 : ${j}`)
 //     }
 // }
 
-// const pokemon=['피카츄','이브이','라이츄','파이리','꼬부기', '이상해씨', '버터플', '야도란', '피존투', '또가스', '고스트']
+const pokemon=['피카츄','이브이','라이츄','파이리','꼬부기', '이상해씨', '버터플', '야도란', '피존투', '또가스', '고스트']
 
 //pokemon.length로 배열의 길이를 가져올 수 있다.
 // for(let i=0;i<=pokemon.length; i++){
@@ -19,6 +26,7 @@ console.log('for문 연습')
 
 
 //구구단
+// 1단 혹은 2단부터 9단까지 출력
 const style=`background-color:red; color:white; padding: 2px 8px; font-size:18px; font-weight: bold;`
 // %c =스타일 인식
 for(let i=2; i<=9; i++){
---
+++ 230920/javascript/array.js
@@ -0,0 +1,22 @@
+//array.js -배열
+
+const number=[1,2,3]
+const animal=['고양이', '강아지', '코끼리']
+
+//number 배열의 길이 출력
+//todo:변수명 일치 시킬것
+//console.log(number.length)
+
+//객체, object
+const classRoom={
+    number:'202',
+    students: '20',
+    aircon:true,
+    computer:true
+}
+
+//classRoom 객체에서 number라는 키를 이용해 값을 출력한 상황
+console.log(classRoom.number)
+
+//classRoom의 학생수를 출력해주세요
+console.log(classRoom["students"])
