4a02e7d3a5c39aa16e505bed8fb66779f0091aa7
qkqtkfkd <ruby563@hanmail.net>
2023-09-21 12:34:20 +0900

    반복문과 클릭이벤트

--- 230921/메뉴/script.js
+++ 230921/메뉴/script.js
@@ -8,20 +8,25 @@
 const menuItem = document.querySelectorAll('.menu-item')
 console.log(menuItem)
 
+//변수의 스코프(scope, 범위, 영역)
+//let 키워드로 선언된 변수의 scope
+//변수를 선언할 때 원칙-동일한 이름으로 선언할 수 없다.
+//scope 다르다면, 동일한 이름을 사용해도, 영역이 다르기 때문에 가능.
+
 for(let i=0; i<menuItem.length; i++){
     //console.log(i)
     console.log(menuItem[i])
+
     //각 버튼을 누를 때마다 'active'클래스가 추가되도록 addEventListener를 붙여서 작성해주세요
     menuItem[i].addEventListener('click',()=>{
         //클릭했을 때, 모든 요소에서 'active'클래스 제거
-        for(let i=0; i<menuItem.length; i++){
-            menuItem[i].classList.remove('active')
+        for(let button=0; button<menuItem.length; button++){
+            menuItem[button].classList.remove('active')
         }
-
         menuItem[i].classList.add('active')
-
     })
 }
+
 
 
 //마지막 번째??
