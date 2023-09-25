086adbccda21ac6ab02562c97a6401841d31428a
qkqtkfkd <ruby563@hanmail.net>
2023-09-25 17:23:32 +0900

    mouse move 이벤트

---
+++ 230925/마우스무브/script.js
@@ -0,0 +1,32 @@
+console.log('running...')
+const dot = document.querySelector('.dot')
+console.log(dot)
+
+
+window.addEventListener('mousemove', (event) => {
+    // console.log(`X좌표 : ${event.clientX}`)
+    // console.log(`Y좌표 : ${event.clientY}`)
+    // console.log(`dot의 width: ${dot.clientWidth / 2}`)
+    // console.log(`dot의 hight: ${dot.clientWidth / 2}`)
+    requestAnimationFrame(() => {
+        mouseMove(event)
+    })
+})
+
+const mouseMove = ((event) => {
+    dot.style.left = event.clientX - (dot.clientWidth / 2) + 'px'
+    dot.style.top = event.clientY - (dot.clientWidth / 2) + 'px'
+
+})
+
+
+window.addEventListener('click', () => {
+    //dot에게 bubble 클래스를 가진 자식요소 추가
+    const span = document.createElement('span')
+    span.classList.add('bubble');
+    dot.append(span)
+    //요소를 삭제하는 메소드
+    setTimeout(() => {
+        span.remove()
+    }, 5000)
+})
