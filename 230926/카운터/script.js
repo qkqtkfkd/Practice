2ff8166f428c18be0aaa47580e71a8d18d4a55e7
qkqtkfkd <ruby563@hanmail.net>
2023-09-26 12:27:47 +0900

    counter 예제 완성

---
+++ 230926/카운터/script.js
@@ -0,0 +1,22 @@
+const dataList = document.querySelectorAll('[data-counter]')
+console.log(dataList)
+
+//반복문을 이용해서 dataList 안에 있는 data-counter 값을 콘솔로 출력
+
+for (let i = 0; i < dataList.length; i++) {
+    const target = dataList[i].dataset.counter
+    const timerText = dataList[i].querySelector('.timer')
+    const step = Math.floor(target / 200)
+    console.log(step)
+
+    let index = 0;
+    setInterval(() => {
+        if (index < target) {
+            //증가
+            index = index + step
+            console.log(index)
+            timerText.textContent = index
+        }
+    }, 1)
+
+}
